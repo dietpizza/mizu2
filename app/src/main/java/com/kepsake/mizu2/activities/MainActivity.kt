59ec6c91536c0fb196348152ad810730bd356ccf
Rohan Biswas <=>
2025-03-21 02:19:36 +0530

    baby steps

--- app/src/main/java/com/kepsake/mizu2/activities/MainActivity.kt
+++ app/src/main/java/com/kepsake/mizu2/activities/MainActivity.kt
@@ -1,21 +1,40 @@
 package com.kepsake.mizu2.activities
 
+import android.graphics.Rect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.recyclerview.widget.GridLayoutManager
+import com.kepsake.mizu2.adapters.MangaCardImageAdapter
 import com.kepsake.mizu2.databinding.ActivityMainBinding
 
+
 class MainActivity : ComponentActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    val imageUrls = (10..90).map { "http://192.168.0.105:3000/image_0${it}.png" }
 
     fun init() {
         initTopLoader()
+        initGridView()
     }
 
     fun initTopLoader() {
         binding.pullToRefresh.setProgressViewOffset(true, 0, 200)
         binding.pullToRefresh.isEnabled = false
-        binding.pullToRefresh.isRefreshing = true
+//        binding.pullToRefresh.isRefreshing = true
+    }
+
+
+    fun initGridView() {
+        val adapter = MangaCardImageAdapter(imageUrls)
+        val layoutManager = GridLayoutManager(this, 3)
+
+        binding.mangaList.layoutManager = layoutManager
+        binding.mangaList.adapter = adapter
+
+        binding.mangaList.setPadding(0, getStatusBarHeight(), 0, 0)
+        binding.mangaList.clipToPadding = false
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,4 +52,13 @@ class MainActivity : ComponentActivity() {
             init()
         }
     }
+
+    private fun getStatusBarHeight(): Int {
+        val rect = Rect()
+        window.decorView.getWindowVisibleDisplayFrame(rect)
+        val statusBarHeight: Int = rect.top
+
+        return statusBarHeight
+    }
+
 }
--- app/src/main/java/com/kepsake/mizu2/adapters/MangaCardImageAdapter.kt
+++ app/src/main/java/com/kepsake/mizu2/adapters/MangaCardImageAdapter.kt
@@ -1,2 +1,31 @@
 package com.kepsake.mizu2.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.kepsake.mizu2.databinding.WidgetMangaCardBinding
+
+
+class MangaCardImageAdapter(private val imageUrls: List<String>) :
+    RecyclerView.Adapter<MangaCardImageAdapter.MangaCardImageViewHolder>() {
+
+    class MangaCardImageViewHolder(val binding: WidgetMangaCardBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MangaCardImageViewHolder {
+        val binding =
+            WidgetMangaCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MangaCardImageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MangaCardImageViewHolder, position: Int) {
+        val imageUrl = imageUrls[position]
+
+        holder.binding.gridImageView.load(imageUrl) {
+            crossfade(true)
+        }
+    }
+
+    override fun getItemCount(): Int = imageUrls.size
+}
