16cb951486b699c4b6c3b81ec063b8acfa61cdad
Rohan Biswas <=>
2025-03-29 17:14:49 +0530

    this is needed

--- app/src/main/java/com/kepsake/mizu2/activities/MangaReaderActivity.kt
+++ app/src/main/java/com/kepsake/mizu2/activities/MangaReaderActivity.kt
@@ -9,6 +9,7 @@ import com.kepsake.mizu2.data.viewmodels.MangaFileViewModel
 import com.kepsake.mizu2.data.viewmodels.MangaPanelViewModel
 import com.kepsake.mizu2.databinding.ActivityMangaReaderBinding
 import com.kepsake.mizu2.helpers.MangaReaderUIHelper
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MangaReaderActivity : ComponentActivity() {
@@ -32,6 +33,13 @@ class MangaReaderActivity : ComponentActivity() {
         binding = ActivityMangaReaderBinding.inflate(layoutInflater)
         setContentView(binding.root)
         vMangaFile.loadMangaFileById(mangaId)
+
+        lifecycleScope.launch {
+            binding.bottomAppBar.apply {
+                delay(10)
+                performHide(false)
+            }
+        }
 
         initializeApp()
     }
